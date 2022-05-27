39a36ad5447992f93c03efbaaabfa13258c908db
Eythor Helgason <eythor@Eythors-MacBook-Pro.local>
2022-05-27 14:05:06 +0000

    Profile implemetation started

--- user/views.py
+++ user/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+from user.forms.profile_form import ProfileForm
+from user.models import UserDetails;
 
 
 def register(request):
@@ -12,3 +16,21 @@ def register(request):
     return render(request, 'pages/user/register.html', {
         'form': UserCreationForm()
     })
+
+
+@login_required
+def profile(request):
+    user = UserDetails.objects.filter(user=request.user).first()
+
+    if request.method == 'POST':
+        form = ProfileForm(instance=user, data=request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.user = request.user
+            user.save()
+            return redirect('profile')
+
+    return render(request, 'pages/user/profile.html', {
+        'form': ProfileForm(instance=user)
+    })
+
